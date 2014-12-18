1852d1ffffd7c6098f47de6bd13c7612e6a2bd6f
Dylan Wang <dylan.wang@gmail.com>
2014-12-18 12:29:11 -0800

    Branch base; not entirely working yet.

--- Planetary Explorers/PEGame.cs
+++ Planetary Explorers/PEGame.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Planetary_Explorers.PlanetView;
 using Planetary_Explorers.SpaceMap;
 using SFML;
 using SFML.Audio;
@@ -56,11 +57,15 @@ namespace Planetary_Explorers
             var d = new Display(ScreenSize);
             _gm = new GameManager(_window, d)
             {
+                
                 ActiveDisplayRoot = new SpaceGrid(new Vector2u(2000, 2000), new Vector2u(800, 500))
                 {
                     Position = new Vector2f(40,90)
                 }
-            };           
+                
+                //ActiveDisplayRoot = new PlanetDisplay(new Vector2u(600,500))
+            };
+            //((PlanetDisplay)_gm.ActiveDisplayRoot).FocusedPlanet = new Planet(_gm.ActiveDisplayRoot);
         }
 
         public void Run()
---
+++ Planetary Explorers/PlanetView/PlanetDisplay.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Planetary_Explorers.SpaceMap;
+using SFML.Graphics;
+using SFML.Window;
+
+namespace Planetary_Explorers.PlanetView
+{
+    class PlanetDisplay : Display
+    {
+        private readonly CircleShape _planetCircle;
+
+        private Planet _focusedPlanet;
+        public Planet FocusedPlanet
+        {
+            get { return _focusedPlanet; }
+            set
+            {
+                _focusedPlanet = value;
+                _planetCircle.Texture = _focusedPlanet.SurfaceTexture;
+                //_planetCircle.Texture.Repeated = true;
+            }
+        }
+
+        public PlanetDisplay(Vector2u displaySize) : base(displaySize)
+        {
+            _planetCircle = new CircleShape()
+            {
+                Radius = 64,
+                Origin = new Vector2f(32,32),
+                OutlineColor = Color.Black,
+                OutlineThickness = 3,
+                Position = new Vector2f(96,96),
+            };
+
+            AddItemToDraw(_planetCircle, 1);
+
+            OnMousePress += delegate(object sender, MouseButtonEventArgs args, Vector2f coords)
+            {
+                if (args.Button == Mouse.Button.Middle)
+                {
+                    _planetCircle.Texture = Planet.GeneratePlanetTexture(new Vector2u(32, 32));
+                    _planetCircle.TextureRect = new IntRect(0, 0, 16, 16);
+                }
+            };
+        }
+        public override void Update()
+        {
+           _planetCircle.TextureRect = new IntRect(_planetCircle.TextureRect.Left + 1,
+               0, _planetCircle.TextureRect.Width, _planetCircle.TextureRect.Height);
+            
+            base.Update();
+        }
+    }
+}
