86082b7cd96d0f01cd9aaf08a64a443be98df923
Dylan Wang <dylan.wang@gmail.com>
2014-12-19 04:38:43 -0800

    Planet textures are now seamless; we can now have planets that scroll textures.

--- Planetary Explorers/PEGame.cs
+++ Planetary Explorers/PEGame.cs
@@ -44,7 +44,6 @@ namespace Planetary_Explorers
             _window.SetVerticalSyncEnabled(false);
             _window.Closed += window_Closed;
 
-
             _targetSpr = new Sprite();
 
             _target = new RenderTexture(ScreenSize.X, ScreenSize.Y)
@@ -57,15 +56,15 @@ namespace Planetary_Explorers
             var d = new Display(ScreenSize);
             _gm = new GameManager(_window, d)
             {
-                
+                /*
                 ActiveDisplayRoot = new SpaceGrid(new Vector2u(2000, 2000), new Vector2u(800, 500))
                 {
                     Position = new Vector2f(40,90)
                 }
-                
-                //ActiveDisplayRoot = new PlanetDisplay(new Vector2u(600,500))
+                */
+                ActiveDisplayRoot = new PlanetDisplay(new Vector2u(600,500))
             };
-            //((PlanetDisplay)_gm.ActiveDisplayRoot).FocusedPlanet = new Planet(_gm.ActiveDisplayRoot);
+            ((PlanetDisplay)_gm.ActiveDisplayRoot).FocusedPlanet = new Planet(_gm.ActiveDisplayRoot);
         }
 
         public void Run()
--- Planetary Explorers/PlanetView/PlanetDisplay.cs
+++ Planetary Explorers/PlanetView/PlanetDisplay.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -21,7 +22,7 @@ namespace Planetary_Explorers.PlanetView
             {
                 _focusedPlanet = value;
                 _planetCircle.Texture = _focusedPlanet.SurfaceTexture;
-                //_planetCircle.Texture.Repeated = true;
+                _planetCircle.TextureRect = new IntRect(0, 0, (int)Math.Round(_planetCircle.Texture.Size.X/2f), (int)Math.Round(_planetCircle.Texture.Size.Y/2f));
             }
         }
 
@@ -40,18 +41,23 @@ namespace Planetary_Explorers.PlanetView
 
             OnMousePress += delegate(object sender, MouseButtonEventArgs args, Vector2f coords)
             {
-                if (args.Button == Mouse.Button.Middle)
-                {
-                    _planetCircle.Texture = Planet.GeneratePlanetTexture(new Vector2u(32, 32));
-                    _planetCircle.TextureRect = new IntRect(0, 0, 16, 16);
-                }
+
             };
         }
         public override void Update()
         {
-           _planetCircle.TextureRect = new IntRect(_planetCircle.TextureRect.Left + 1,
-               0, _planetCircle.TextureRect.Width, _planetCircle.TextureRect.Height);
-            
+            //TODO: Precalculate and store the width/height. It should not change
+            _planetCircle.TextureRect = new IntRect(_planetCircle.TextureRect.Left + 1,
+                _planetCircle.TextureRect.Top+1, (int)Math.Round(_planetCircle.Texture.Size.X / 2f), (int)Math.Round(_planetCircle.Texture.Size.Y / 2f));
+
+            if (_planetCircle.TextureRect.Left > _planetCircle.Texture.Size.X / 2f)
+            {
+                _planetCircle.TextureRect = new IntRect(0, _planetCircle.TextureRect.Top, (int)Math.Round(_planetCircle.Texture.Size.X / 2f), (int)Math.Round(_planetCircle.Texture.Size.Y / 2f));
+            }
+            if (_planetCircle.TextureRect.Top > _planetCircle.Texture.Size.Y / 2f)
+            {
+                _planetCircle.TextureRect = new IntRect(_planetCircle.TextureRect.Left, 0, (int)Math.Round(_planetCircle.Texture.Size.X / 2f), (int)Math.Round(_planetCircle.Texture.Size.Y / 2f));
+            }
             base.Update();
         }
     }
